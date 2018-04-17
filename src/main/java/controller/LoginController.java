c4e5ff5e99e0f331cab62ba985cb6eb8ebdac830
mrezlik <mrezlik@gmail.com>
2018-04-17 14:40:24 +0200

    Implement "POST" methot handling

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -5,25 +5,42 @@ import com.sun.net.httpserver.HttpHandler;
 import dao.LoginDao;
 import view.LoginView;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URLDecoder;
+import java.sql.SQLException;
+import java.util.*;
 
 
-public class UserController implements HttpHandler {
+public class LoginController implements HttpHandler {
 
     private LoginView view;
 
-    public UserController() {
+    public LoginController() {
         view = new LoginView();
     }
 
     public void handle(HttpExchange httpExchange) throws IOException {
         String response = view.getLoginScreen();
-        httpExchange.sendResponseHeaders(200, response.length());
-        OutputStream os = httpExchange.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
-    }
+        String method = httpExchange.getRequestMethod();
+
+        if (method.equals("POST")) {
+
+            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            String formData = br.readLine();
+            Map<String, String> inputs = parseFormData(formData);
+            String login = inputs.get("login");
+            String password = inputs.get("password");
+            System.out.println("User type: " + logInUser(login, password) +
+            "\nUser login: " + login);
+        }
+
+            httpExchange.sendResponseHeaders(200, response.length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+
 
     private String logInUser(String login, String password) {
         LoginDao loginDao = new LoginDao();
@@ -39,6 +56,19 @@ public class UserController implements HttpHandler {
     private int getLoginId(String login, String password) {
         LoginDao loginDao = new LoginDao();
         return loginDao.findLoginId(login, password);
+    }
+
+    private static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = formData.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            // We have to decode the value because it's urlencoded. see: https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
+            new URLDecoder();
+            String value = URLDecoder.decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+        return map;
     }
 
     public void startApplication() {
--- src/main/java/controller/RootController.java
+++ src/main/java/controller/RootController.java
@@ -10,12 +10,12 @@ public class RootController {
 
     public static void main(String[] args) {
 
-//        new UserController().startApplication();
+//        new LoginController().startApplication();
 
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
-            server.createContext("/login", new UserController());
+            server.createContext("/login", new LoginController());
             server.createContext("/static", new Static());
             server.setExecutor(null);
 
