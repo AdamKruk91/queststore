4ca56f022a5d2fe3705a265f0492d9e4c2a95526
Piotr Górski <piotrek.gorski90@gmail.com>
2018-04-18 10:45:35 +0200

    Add level model and dao interface

---
+++ src/main/java/dao/LevelDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import model.Iterator;
+import model.Level;
+
+import java.sql.SQLException;
+
+public interface LevelDAO {
+    void addLevel(String name, int exp) throws SQLException;
+    void deleteLevel(int id) throws SQLException;
+    Iterator getLevels() throws SQLException;
+    Level getLevel(int totalExp) throws SQLException;
+}
