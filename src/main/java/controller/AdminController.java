835e36251d60f1d6868b0488618ad12cf2e82d84
AdamKruk91 <adam.kruk91@gmail.com>
2018-04-19 23:25:09 +0200

    Delete unnecessary methods & add creating mentors methods

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -78,17 +78,48 @@ public class AdminController extends AbstractContoller implements HttpHandler {
         }
     }
 
+    private void renderProfile(HttpExchange httpExchange, int loginID) throws IOException {
+        AdminModel admin = getAdmin(loginID);
+        String response = view.getProfileScreen(admin);
+        handlePositiveResponse(httpExchange, response);
+    }
+
+    private void renderMentorsData(HttpExchange httpExchange) throws IOException {
+        List<MentorModel> allMentors = mentorDao.getAllMentorsCollection();
+        String response = view.getMentorsDisplay(allMentors);
+        handlePositiveResponse(httpExchange, response);
+    }
+
+    private void handleCreateMentor(HttpExchange httpExchange) throws IOException {
+        String method = httpExchange.getRequestMethod();
+        if(method.equals("POST")){
+            MentorModel mentorModel = createMentorFromISR(httpExchange);
+            try {
+                mentorDao.insertNewMentor(mentorModel);
+                renderCreateMentorWithMessage(httpExchange, "Mentor creation was successful!");
+            } catch (SQLException e){
+                e.printStackTrace();
+                renderCreateMentorWithMessage(httpExchange, "Mentor creation failed!");
+            }
+        } else{
+            renderCreateMentor(httpExchange);
+        }
+    }
+
+    private void renderCreateMentor(HttpExchange httpExchange) throws IOException {
+        String response = view.getCreateMentor(groupDao.getGroupsCollection());
+        handlePositiveResponse(httpExchange, response);
+    }
+
+    private void renderCreateMentorWithMessage(HttpExchange httpExchange, String message) throws IOException {
+        String response = view.getCreateMentorMessage(groupDao.getGroupsCollection(), message);
+        handlePositiveResponse(httpExchange, response);
+    }
+
     private void handleEditMentor(HttpExchange httpExchange) throws IOException{
         String method = httpExchange.getRequestMethod();
         if(method.equals("POST")) {
-            Map<String, String> inputs = getMapFromISR(httpExchange);
-            int id = Integer.parseInt(inputs.get("id"));
-            String firstName = inputs.get("name");
-            String lastName = inputs.get("surname");
-            String email = inputs.get("email");
-            String password = inputs.get("password");
-            int groupId = Integer.parseInt(inputs.get("dropdown"));
-            MentorModel mentorModel = new MentorModel(id, firstName, lastName, email, password, groupId);
+            MentorModel mentorModel = createMentorWithIdFromISR(httpExchange);
             mentorDao.updateMentorTable(mentorModel);
             renderEditMentor(httpExchange);
         } else {
@@ -119,124 +150,34 @@ public class AdminController extends AbstractContoller implements HttpHandler {
         handlePositiveResponse(httpExchange, response);
     }
 
-    private void renderMentorsData(HttpExchange httpExchange) throws IOException {
-        List<MentorModel> allMentors = mentorDao.getAllMentorsCollection();
-        String response = view.getMentorsDisplay(allMentors);
-        handlePositiveResponse(httpExchange, response);
-    }
-
     private void renderCreateGroupWithMessage(HttpExchange httpExchange, String message) throws IOException{
         String response = view.getCreateGroupMessage(message);
         handlePositiveResponse(httpExchange, response);
     }
 
-    private void handleCreateMentor(HttpExchange httpExchange) throws IOException {
-        String method = httpExchange.getRequestMethod();
-        if(method.equals("POST")){
-            Map<String, String> inputs = getMapFromISR(httpExchange);
-            String firstName = inputs.get("name");
-            String lastName = inputs.get("surname");
-            String email = inputs.get("email");
-            String password = inputs.get("password");
-            int groupId = Integer.parseInt(inputs.get("dropdown"));
-            MentorModel mentorModel = new MentorModel(firstName, lastName, email, password, groupId);
-            try {
-                mentorDao.insertNewMentor(mentorModel);
-                renderCreateMentorWithMessage(httpExchange, "Mentor creation was successful!");
-            } catch (SQLException e){
-                e.printStackTrace();
-                renderCreateMentorWithMessage(httpExchange, "Mentor creation failed!");
-            }
-        } else{
-            renderCreateMentor(httpExchange);
-        }
+    private MentorModel createMentorFromISR(HttpExchange httpExchange) throws IOException {
+        Map<String, String> inputs = getMapFromISR(httpExchange);
+        String firstName = inputs.get("name");
+        String lastName = inputs.get("surname");
+        String email = inputs.get("email");
+        String password = inputs.get("password");
+        int groupId = Integer.parseInt(inputs.get("dropdown"));
+        return new MentorModel(firstName, lastName, email, password, groupId);
     }
 
-    private void renderCreateMentor(HttpExchange httpExchange) throws IOException {
-        String response = view.getCreateMentor(groupDao.getGroupsCollection());
-        handlePositiveResponse(httpExchange, response);
-    }
-
-    private void renderCreateMentorWithMessage(HttpExchange httpExchange, String message) throws IOException{
-        String response = view.getCreateMentorMessage(groupDao.getGroupsCollection(), message);
-        handlePositiveResponse(httpExchange, response);
-    }
-
-    private void renderProfile(HttpExchange httpExchange, int loginID) throws IOException {
-        AdminModel admin = getAdmin(loginID);
-        String response = view.getProfileScreen(admin);
-        handlePositiveResponse(httpExchange, response);
+    private MentorModel createMentorWithIdFromISR(HttpExchange httpExchange) throws IOException {
+        Map<String, String> inputs = getMapFromISR(httpExchange);
+        int id = Integer.parseInt(inputs.get("id"));
+        String firstName = inputs.get("name");
+        String lastName = inputs.get("surname");
+        String email = inputs.get("email");
+        String password = inputs.get("password");
+        int groupId = Integer.parseInt(inputs.get("dropdown"));
+        return new MentorModel(id, firstName, lastName, email, password, groupId);
     }
 
     private AdminModel getAdmin(int idLogin) {
         return adminDao.getAdminByIdLogin(idLogin);
-    }
-
-    public void controlMenuOptions() throws SQLException {
-        boolean whileRunning = true;
-        while (whileRunning) {
-            view.displayAdminMenu();
-            int userChoice = inputController.getIntInput("SELECT AN OPTION: ");
-            switch (userChoice) {
-                case 1:
-                    createMentor();
-                    break;
-                case 2:
-                    createGroup();
-                    break;
-                case 3:
-                    editMentorDataPanel();
-                    break;
-                case 4:
-                    displayMentorData();
-                    break;
-                case 5:
-                    deleteMentor();
-                    break;
-                case 6:
-                    try {
-                        manageLevels();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case 0:
-                    whileRunning = false;
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    private GroupModel selectGroup() {
-        GroupDao groupDao = new GroupDao();
-        List<GroupModel> allGroups =groupDao.getGroupsCollection();
-        view.displayAllGroups(allGroups);
-        int id = inputController.getIntInput("Enter id of the chosen group: ");
-        GroupModel selectedGroup = null;
-        for (GroupModel group: allGroups)
-            if (group.getId() == id)
-                selectedGroup = group;
-        return selectedGroup;
-    }
-
-    private void createMentor() throws SQLException {
-        String mentorName = inputController.getStringInput("Enter mentor name: ");
-        String mentorLastName = inputController.getStringInput("Enter mentor last name: ");
-        String mentorEmail = inputController.getStringInput("Enter mentor email: ");
-        String mentorPassword = inputController.getStringInput("Enter mentor password: ");
-        GroupModel selectedGroup = selectGroup();
-        int idGroup = selectedGroup.getId();
-        MentorDao mentorDao = new MentorDao();
-        MentorModel mentor = new MentorModel(mentorName, mentorLastName, mentorEmail, mentorPassword, idGroup);
-        mentorDao.insertNewMentor(mentor);
-    }
-
-    private void createGroup() {
-        String groupName = inputController.getStringInput("Enter group name: ");
-        GroupDao groupDao = new GroupDao();
-        groupDao.addNewGroup(groupName);
     }
 
     private MentorModel selectMentor() {
@@ -249,58 +190,6 @@ public class AdminController extends AbstractContoller implements HttpHandler {
                 matchedMentor = mentor;
         }
         return matchedMentor;
-    }
-
-    private void updateMentorData(MentorModel mentor) {
-        mentorDao.updateMentorTable(mentor);
-    }
-
-    private void updateLoginData(MentorModel mentor, String login, String password) throws SQLException {
-        loginDao.updateLoginTable(mentor, login, password);
-    }
-
-    private void editMentorDataPanel() throws SQLException {
-        MentorModel mentorToEdit = selectMentor();
-        String mentorLogin = mentorToEdit.getEmail();
-        String mentorPassword = mentorToEdit.getPassword();
-        boolean isChoose = true;
-        while (isChoose) {
-            view.displayEditMentorMenu();
-            int userChoice = inputController.getIntInput("Select field number to edit: ");
-            switch (userChoice) {
-                case 1:
-                    String name = inputController.getStringInput("Enter mentor name:");
-                    mentorToEdit.setName(name);
-                    break;
-                case 2:
-                    String lastName = inputController.getStringInput("Enter mentor last name");
-                    mentorToEdit.setLastName(lastName);
-                    break;
-                case 3:
-                    String email = inputController.getStringInput("Enter mentor email");
-                    mentorToEdit.setEmail(email);
-                    break;
-                case 4:
-                    String password = inputController.getStringInput("Enter mentor password");
-                    mentorToEdit.setPassword(password);
-                    break;
-                case 5:
-                    GroupModel groupModel = selectGroup();
-                    mentorToEdit.setIdGroup(groupModel.getId());
-                case 0:
-                    isChoose = false;
-                    break;
-                default:
-                    break;
-            }
-        }
-        updateMentorData(mentorToEdit);
-        updateLoginData(mentorToEdit, mentorLogin, mentorPassword);
-    }
-
-    private void displayMentorData() {
-        MentorModel mentor = selectMentor();
-        view.displayMentorData(mentor);
     }
 
     private void deleteMentor(){
