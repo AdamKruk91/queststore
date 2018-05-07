c8d1565c77136724c4e3f63f01b5773d17e0017b
Piotr Górski <piotrek.gorski90@gmail.com>
2018-05-07 14:45:36 +0200

    Add SessionModel class and import in SessionDaoInterface

--- src/main/java/dao/SessionDaoInterface.java
+++ src/main/java/dao/SessionDaoInterface.java
@@ -1,5 +1,7 @@
 package dao;
 
+import model.SessionModel;
+
 public interface SessionDaoInterface {
     void addSession();
     void deleteSession();
---
+++ src/main/java/model/SessionModel.java
@@ -0,0 +1,5 @@
+package model;
+
+public class SessionModel {
+    private String id;
+}
