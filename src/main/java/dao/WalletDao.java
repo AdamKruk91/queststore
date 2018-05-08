c23f3616d3881b9304bc16d6be0600821b25aaec
AdamKruk91 <adam.kruk91@gmail.com>
2018-05-08 13:21:35 +0200

    Add getArtifactyuserid method in walletdao

--- src/main/java/dao/WalletDao.java
+++ src/main/java/dao/WalletDao.java
@@ -80,8 +80,33 @@ public class WalletDao extends ManipulationDao implements WalletDaoInterface{
     }
 
     @Override
-    public ArrayList<ArtifactModel> getArtifactByUserId(int user_id) {
-        return null;
+    public List<ArtifactModel> getArtifactByUserId(int user_id) throws DataAccessException{
+        try {
+            PreparedStatement ps = getConnection().prepareStatement(
+                    "SELECT user_artifact.id as 'id', artifact.name as 'artifact name', artifact.description as 'description'," +
+                            "artifact.price as 'price', artifact_category.name as 'category name', artifact_status.name as 'status' " +
+                            "FROM user_artifact JOIN artifact ON user_artifact.artifact_id = artifact.id JOIN artifact_status ON " +
+                            " user_artifact.status_id = artifact_status.id JOIN artifact_category ON artifact.category_id = artifact_category.id WHERE" +
+                            " user_artifact.user_id = ?;");
+            ps.setInt(1, user_id);
+            ResultSet rs = ps.executeQuery();
+
+            List<ArtifactModel> userArtifact = new ArrayList<>();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("artifact name");
+                String description = rs.getString("description");
+                int price = rs.getInt("price");
+                String categoryName= rs.getString("category name");
+                String status = rs.getString("status");
+                ArtifactModel artifact = new ArtifactModel(id, name, description, price, categoryName, status);
+                userArtifact.add(artifact);
+            }
+            return userArtifact;
+        } catch (SQLException e){
+            throw new DataAccessException("Problem with getting user artifact");
+        }
     }
 
     @Override
