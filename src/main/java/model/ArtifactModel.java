aa01bf8b9feb940c1cc03e61f885bbb7fa2a3d3e
AdamKruk91 <adam.kruk91@gmail.com>
2018-05-08 13:17:01 +0200

    Add status string in artifact model

--- src/main/java/model/ArtifactModel.java
+++ src/main/java/model/ArtifactModel.java
@@ -4,15 +4,18 @@ package model;
 public class ArtifactModel extends UsableObjectModel {
 
     private int price;
+    private String status;
 
-    public ArtifactModel(int id, String name, String description, int price, String category) {
-        super(id, name, description, category);
-        this.price = price;
-    }
-
-    public ArtifactModel(String name, String description, int price, String category) {
+    public ArtifactModel(String name, String description, int price, String category, String status) {
         super(name, description, category);
         this.price = price;
+        this.status = status;
+    }
+
+    public ArtifactModel(int id, String name, String description, int price, String category, String status) {
+        super(id, name, description, category);
+        this.price = price;
+        this.status = status;
     }
 
     public int getPrice() {
