e5767179d3da9596262ef0a060f3e2db1845fda9
AdamKruk91 <adam.kruk91@gmail.com>
2018-05-08 13:18:48 +0200

    Add adding and updating wallet in wallet dao

--- src/main/java/dao/WalletDao.java
+++ src/main/java/dao/WalletDao.java
@@ -1,21 +1,44 @@
 package dao;
 
+import exceptions.DataAccessException;
 import model.ArtifactModel;
-import model.Iterator;
 import model.WalletModel;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class WalletDao extends ManipulationDao implements WalletDaoInterface{
 
     @Override
-    public void addWallet(WalletModel wallet) {
-
+    public void addWallet(WalletModel wallet) throws DataAccessException{
+        try {
+            PreparedStatement ps = getConnection().prepareStatement(
+                    "INSERT INTO wallet (user_id, total_coins_earned, amount) " +
+                            "VALUES(?, ?, ?)");
+            ps.setInt(1, wallet.getID());
+            ps.setInt(2, wallet.getTotalCoinsEarned());
+            ps.setInt(3, wallet.getAmount());
+            ps.executeUpdate();
+        } catch (SQLException e){
+            throw new DataAccessException("Problem with adding new wallet");
+        }
     }
 
     @Override
-    public void updateWallet(WalletModel wallet) {
-
+    public void updateWallet(WalletModel wallet) throws DataAccessException{
+        try {
+            PreparedStatement ps = getConnection().prepareStatement(
+                    "UPDATE wallet SET total_coins_earned=?, amount=? WHERE user_id=?;");
+            ps.setInt(1, wallet.getTotalCoinsEarned());
+            ps.setInt(2, wallet.getAmount());
+            ps.setInt(3, wallet.getID());
+            ps.executeUpdate();
+        } catch (SQLException e){
+            throw new DataAccessException("Problem with updating wallet");
+        }
     }
 
     @Override
