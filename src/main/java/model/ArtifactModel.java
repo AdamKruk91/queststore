780dfb6a818536d46cfee928878e40e8d8608fdd
mrezlik <mrezlik@gmail.com>
2018-05-08 14:53:06 +0200

    Little refactor ArtifactModel

--- src/main/java/model/ArtifactModel.java
+++ src/main/java/model/ArtifactModel.java
@@ -6,16 +6,15 @@ public class ArtifactModel extends UsableObjectModel {
     private int price;
     private String status;
 
-    public ArtifactModel(String name, String description, int price, String category) {
+    public ArtifactModel(String name, String description, int price, String category)
+    {
         super(name, description, category);
         this.price = price;
         this.status = "In shop";
     }
 
-    public ArtifactModel(int id, String name, String description, int price, String category, String status) {
-        super(id, name, description, category);
-        this.price = price;
-        this.status = status;
+    public ArtifactModel(int id, String name, String description, int price, String category) {
+        this(id, name, description, price, category, "In shop");
     }
 
     public ArtifactModel(int id, String name, String description, int price, String category, String status) {
