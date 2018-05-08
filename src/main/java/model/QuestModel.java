2ada7504d8da8ce7ea8a61975a8a4280112ed506
Piotr Górski <piotrek.gorski90@gmail.com>
2018-05-08 10:19:12 +0200

    Refactor QuestModel to match new logic

--- src/main/java/model/QuestModel.java
+++ src/main/java/model/QuestModel.java
@@ -2,13 +2,24 @@ package model;
 
 
 public class QuestModel extends UsableObjectModel {
+    private int reward;
     
-    public QuestModel(int id, String type, String name, String description, int value) {
-        super(id, type, name, description, value);
+    public QuestModel(int id, String name, String description, String category, int reward) {
+        super(id, name, description, category);
+        this.reward = reward;
     }
 
-    public QuestModel(String type, String name, String description, int value) {
-        super(type, name, description, value);
+    public QuestModel(String name, String description,String category, int reward) {
+        super(name, description, category);
+        this.reward = reward;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
     }
 }
     
