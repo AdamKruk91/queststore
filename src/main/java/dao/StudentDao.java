d37c1b6826343146a1d78adda7848118424ac251
Piotr Górski <piotrek.gorski90@gmail.com>
2018-05-08 13:42:47 +0200

    Add try catch blocks and add student method

--- src/main/java/dao/StudentDao.java
+++ src/main/java/dao/StudentDao.java
@@ -5,6 +5,8 @@ import model.GroupModel;
 import model.WalletModel;
 import model.StudentModel;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class StudentDao extends ManipulationDao implements StudentDaoInterface {
@@ -15,27 +17,55 @@ public class StudentDao extends ManipulationDao implements StudentDaoInterface {
 
     @Override
     public StudentModel getStudentById(int id) throws DataAccessException {
-        return null;
+        try{
+
+        }catch (SQLException e) {
+            throw new DataAccessException("Get student failed!");
+        }
     }
 
     @Override
     public List<StudentModel> getStudentsCollection() throws DataAccessException {
-        return null;
+        try{
+
+        }catch (SQLException e) {
+            throw new DataAccessException("Getting student collection failed!");
+        }
     }
 
     @Override
     public void addStudent(StudentModel student) throws DataAccessException {
-
+        try {
+            PreparedStatement ps = getConnection().prepareStatement(
+                    "INSERT INTO user (login, password, name, surname, email, user_category_id) " +
+                            "VALUES(?, ?, ?, ?, ?, ?)");
+            ps.setString(1, student.getLogin());
+            ps.setString(2, student.getPassword());
+            ps.setString(3, student.getName());
+            ps.setString(4, student.getSurname());
+            ps.setString(5, student.getEmail());
+            ps.setInt(6, USER_CATEGORY_ID);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Add student failed!");
+        }
     }
-
     @Override
     public void deleteStudent(int id) throws DataAccessException {
+        try{
 
+        }catch (SQLException e) {
+            throw new DataAccessException("Remove student failed!");
+        }
     }
 
     @Override
     public void updateStudent(StudentModel student) throws DataAccessException {
+        try{
 
+        }catch (SQLException e) {
+            throw new DataAccessException("Update student failed!");
+        }
     }
 
     @Override
