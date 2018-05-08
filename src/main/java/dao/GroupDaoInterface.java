cd0460ba6623407913e3ec2a4cd4f7e67b9610d1
Piotr Górski <piotrek.gorski90@gmail.com>
2018-05-08 13:56:59 +0200

    Update GroupDaoInterface

--- src/main/java/dao/GroupDaoInterface.java
+++ src/main/java/dao/GroupDaoInterface.java
@@ -9,4 +9,5 @@ public interface GroupDaoInterface {
     void addNewGroup(GroupModel group) throws DataAccessException;
     List<GroupModel> getGroupsCollection() throws DataAccessException;
     void removeGroup(GroupModel group) throws DataAccessException;
+    GroupModel getByID(int id) throws DataAccessException;
 }
