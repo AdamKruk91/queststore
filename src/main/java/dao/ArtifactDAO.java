2c7a664008e04c626ec3721d84ff1fb7529ea38a
Piotr Górski <piotrek.gorski90@gmail.com>
2018-05-09 09:43:53 +0200

    Update method names in Artifact

--- src/main/java/dao/ArtifactDAO.java
+++ src/main/java/dao/ArtifactDAO.java
@@ -9,9 +9,9 @@ public interface ArtifactDAO {
 
     Artifact getArtifactById(int artifactID) throws DataAccessException;
     List<Artifact> getArtifactCollection() throws DataAccessException;
-    void addArtifact(Artifact artifact) throws DataAccessException;
-    void removeArtifact(Artifact artifact) throws DataAccessException;
-    void updateArtifact(Artifact artifact) throws DataAccessException;
+    void add(Artifact artifact) throws DataAccessException;
+    void remove(Artifact artifact) throws DataAccessException;
+    void update(Artifact artifact) throws DataAccessException;
     List<Artifact> getUserUnusedArtifacts(int userID) throws DataAccessException;
     List<Artifact> getUserUsedArtifacts(int userID) throws DataAccessException;
     List<Artifact> getUserRequestedArtifacts(int userID) throws DataAccessException;
