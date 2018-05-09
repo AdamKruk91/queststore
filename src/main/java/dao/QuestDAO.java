6baa22711dd955ff24a356fe56dd7cfb8bcd6370
AdamKruk91 <adam.kruk91@gmail.com>
2018-05-09 09:58:26 +0200

    Change method signatures in QuestDAO & SQL

--- src/main/java/dao/QuestDAO.java
+++ src/main/java/dao/QuestDAO.java
@@ -6,10 +6,10 @@ import model.Quest;
 import java.util.List;
 
 public interface QuestDAO {
-    void addQuest(Quest newQuest) throws DataAccessException;
-    void removeQuest(Quest removeQuest) throws DataAccessException;
-    void updateQuest(Quest updateQuest) throws DataAccessException;
+    void add(Quest newQuest) throws DataAccessException;
+    void remove(Quest removeQuest) throws DataAccessException;
+    void update(Quest updateQuest) throws DataAccessException;
     List<String> getCategoriesName() throws DataAccessException;
-    Quest getByID(int id) throws DataAccessException;
+    Quest get(int id) throws DataAccessException;
     void updateUserQuest(int user_id, int quest_id) throws DataAccessException;
 }
