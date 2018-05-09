50d48e0fdb31a01ec302c180d0e11845546c5d5d
Piotr Górski <piotrek.gorski90@gmail.com>
2018-05-09 11:17:47 +0200

    Update StudentView

--- src/main/java/view/StudentView.java
+++ src/main/java/view/StudentView.java
@@ -8,47 +8,13 @@ import java.util.List;
 
 public class StudentView {
 
-    public void displayStudentMenu(){
-        System.out.println("1 - See your wallet.\n"
-                + "2 - Buy artifact.\n"
-                + "3 - Buy artifact together with teammates.\n"
-                + "4 - See your level of experience\n"
-                + "5 - Exit");
-    }
-
-    public void displayCollectionOfItem(List<Artifact> itemCollection) {
-        System.out.println("\nAvailable artifacts: ");
-        for (UsableObject item: itemCollection) {
-            System.out.println(item.getID() + ". " + item.getName() + " value: " + item.getValue());
-        }
-        System.out.println("\n");
-    }
-
-    public void displayBoughtArtifacts(List<Artifact>  itemCollection) {
-        System.out.println("YOUR ARTIFACTS: ");
-        for (UsableObject item: itemCollection) {
-            System.out.println("==> " + item.getName() + item.getValue());
-        }
-        System.out.println("\n");
-    }
-
-    public void displayWallet(Wallet wallet) {
-        System.out.println("\nBALANCE: " + wallet.getBalance() +
-                           "\nTOTAL COOLCOINS: " + wallet.getTotalCoolcoins());
-    }
-
-    public void displayCurrentExperience(int totalExp, String levelName) {
-        System.out.println(String.format("You gained: %d experience so far!" +
-                                         "\nYour level: %s", totalExp, levelName));
-    }
-
     public String getProfileScreen (Student student, Level level) {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/student-profile.twig");
         JtwigModel model = JtwigModel.newModel();
         model.with("name", student.getFullName());
         model.with("email", student.getEmail());
-        model.with("group", student.getGroup().getGroupName());
-        int totalExp = student.getMyWallet().getTotalCoolcoins();
+        model.with("group", student.getGroup().getName());
+        int totalExp = student.getWallet().getTotalCoinsEarned();
         String levelStr = String.format("%s (%d EXP)", level.getName(), totalExp);
         model.with("level", levelStr); // Todo add reading level
         return template.render(model);
@@ -59,7 +25,7 @@ public class StudentView {
         JtwigModel model = JtwigModel.newModel();
 
         model.with("fullname", student.getFullName());
-        model.with("coins", student.getMyWallet().getBalance());
+        model.with("coins", student.getWallet().getAmount());
         model.with("artifact_list", artifacts);
         return template.render(model);
     }
@@ -69,7 +35,7 @@ public class StudentView {
         JtwigModel model = JtwigModel.newModel();
 
         model.with("fullname", student.getFullName());
-        model.with("coins", student.getMyWallet().getBalance());
+        model.with("coins", student.getWallet().getAmount());
         model.with("artifact_list", artifacts);
         return template.render(model);
     }
@@ -79,7 +45,7 @@ public class StudentView {
         JtwigModel model = JtwigModel.newModel();
 
         model.with("fullname", student.getFullName());
-        model.with("coins", student.getMyWallet().getBalance());
+        model.with("coins", student.getWallet().getAmount());
         model.with("artifact_list", artifacts);
         return template.render(model);
     }
