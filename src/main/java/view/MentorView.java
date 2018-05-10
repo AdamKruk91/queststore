2ca175acd28722e10a5f28eb6e08689d665a77f6
mrezlik <mrezlik@gmail.com>
2018-05-10 11:47:43 +0200

    Implement getRequestScreen method in MentorView

--- src/main/java/view/MentorView.java
+++ src/main/java/view/MentorView.java
@@ -1,5 +1,25 @@
 package view;
 
+import model.Group;
+import model.Mentor;
+import model.Student;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MentorView {
 
-}
+    public String getRequestScreen(Mentor mentor) {
+            JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentor-request.twig");
+            JtwigModel model = JtwigModel.newModel();
+            List<Group> groups = mentor.getGroup();
+            List<Student> students = new ArrayList();
+            for(Group group : groups) {
+                students.addAll(group.getStudents());
+            }
+            model.with("student_list", students);
+            return template.render(model);
+        }
+    }
